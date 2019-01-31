e0e0ba5e38f37243852f1c89716c5fdb338cb25c
黑码哥 <15754713678@163.com>
2019-01-31 16:11:51 +0800

    1

--- CircleOfFriendsDisplay/小视频/小视频/DFVideoPreViewController.m
+++ CircleOfFriendsDisplay/小视频/小视频/DFVideoPreViewController.m
@@ -13,11 +13,14 @@
 
 @interface DFVideoPreViewController()
 
-#define SHOWRATE   1
-#define Press_Button_Height 80*SHOWRATE
+#define SHOWRATE   1.1
+
+#define Press_Button_Height 78*SHOWRATE
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
 #define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
 #define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @property (strong, nonatomic) AVPlayer *player;
 @property (nonatomic, strong) UIButton *cancelButton;
